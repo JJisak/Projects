6195da60df10eb8f92e1e2641370e65ea27b90d5
JJisak <76518907+JJisak@users.noreply.github.com>
2022-01-08 15:06:51 -0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,73 @@
+--select * 
+--from [Profolio Project].dbo.COVID_DEATH
+--order by 3,4
+
+Select location, date, total_cases, new_cases, total_deaths, population
+from [Profolio Project].dbo.COVID_DEATH
+order by 1,2
+
+--Total cases vs Total Deaths
+Select Location, date, total_cases, new_cases, total_deaths, population
+From [Profolio Project].dbo.COVID_DEATH
+Where continent is not null 
+order by 1,2
+
+
+-- Total Cases vs Total Deaths
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From [Profolio Project].dbo.COVID_DEATH
+Where location like '%states%'
+and continent is not null 
+order by 1,2
+
+
+-- Total Cases vs Population
+
+
+Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
+From [Profolio Project].dbo.COVID_DEATH
+Where location like '%states%'
+order by 1,2
+
+
+-- Countries with Highest Infection Rate compared to Population
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From [Profolio Project].dbo.COVID_DEATH
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- Countries with Highest Death Count per Population
+
+Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From [Profolio Project].dbo.COVID_DEATH
+--Where location like '%states%'
+Where continent is not null 
+Group by Location
+order by TotalDeathCount desc
+
+
+
+-- Highest death count per population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From [Profolio Project].dbo.COVID_DEATH
+--Where location like '%states%'
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+-- GLOBAL NUMBERS
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From [Profolio Project].dbo.COVID_DEATH
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+
+
